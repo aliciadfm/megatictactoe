80d1a27289527341a6c04d29f32e25bb57f8b080
Alicia <dfmalicia@gmail.com>
2023-12-02 18:43:38 +0100

    x and o by turns

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 createBoard();
+placePiece();
 
+
+var count = 0;
 
 // printing the 9x9 board
 function createBoard() {
     let bigbox = document.getElementById("bigbox");
     for(let i = 1; i <= 9; i++) {
-        let quadrant = document.createElement('div');
+        let quadrant;
+        quadrant = document.createElement('div');
         quadrant.className = 'quadrant';
         quadrant.id = i;
         for(let j = 1; j <= 9; j++) {
-            let subQuadrant = document.createElement('div');
+            let subQuadrant;
+            subQuadrant = document.createElement('div');
             subQuadrant.className = 'quadrant';
-            subQuadrant.id = i*j;
+            subQuadrant.id = i.toString() + j.toString();
             quadrant.appendChild(subQuadrant);
         }
         bigbox.appendChild(quadrant);
     }
+}
+
+function placePiece() {
+    let bigbox = document.getElementById("bigbox");
+    bigbox.addEventListener('click', function () {
+        let subQuadrant = event.target;
+        if (subQuadrant.classList.contains('quadrant')) {
+            subQuadrant.textContent.className = 'piece';
+            if(count % 2 == 0) {
+                subQuadrant.textContent = "X";
+            } else {
+                subQuadrant.textContent = "O";
+            }
+            count++;
+        }
+    });
+
 }
 
